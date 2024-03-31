470922cf065f3761be642afed00e1d91d4dc0590
Ekaterina Polushina <terryrinkelly@gmail.com>
2024-03-31 21:28:37 +0300

    Добавлено решение по тестам

--- tests/tests_homework.py
+++ tests/tests_homework.py
@@ -108,3 +108,4 @@ def go_to_companyname_homepage(page_url):
 def find_registration_button_on_login_page(page_url, button_text):
     actual_result = beaty_result(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
+
